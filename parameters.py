72b28bad54a75c13e6edb0041c83b4b4fc7396c1
ocksumoron <ocksumoron@yandex.ru>
2021-06-17 01:01:52 +0300

    minor

--- parameters.py
+++ parameters.py
@@ -176,13 +176,6 @@ def get_args():
         type=str2bool, 
         help="if True, student and teacher are sampled from N(0, 1)/sqrt(n), and data from N(0, 1)"
     )
-
-    parser.add_argument(
-        "--reverse_mult_by_sqrt", 
-        default=False, 
-        type=str2bool, 
-        help="if True, student and teacher are sampled from N(0, 1)/sqrt(n), and data from N(0, 1)"
-    )
     
     parser.add_argument(
         "--compute_grad_dif", 
